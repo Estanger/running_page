35e8344a8a12f1a7a01ca8e9c0f4247036dfe3c2
NOBOBY\Nobody <laijing9951@gmail.com>
2026-03-28 19:04:13 +0800

    feat: 配置 Strava 同步和个性化设置

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -15,10 +15,10 @@ const getBasePath = () => {
 };
 
 const data: ISiteMetadataResult = {
-  siteTitle: 'Running Page',
-  siteUrl: 'https://yihong.run',
+  siteTitle: 'Estanger Running',
+  siteUrl: 'https://estanger.github.io/running_page',
   logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
-  description: 'Personal site and blog',
+  description: 'Estanger 的跑步记录',
   navLinks: [
     {
       name: 'Summary',
@@ -26,11 +26,11 @@ const data: ISiteMetadataResult = {
     },
     {
       name: 'Blog',
-      url: 'https://github.com/yihong0618/gitblog',
+      url: 'https://estanger.github.io',
     },
     {
       name: 'About',
-      url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
+      url: 'https://estanger.github.io/about',
     },
   ],
 };
